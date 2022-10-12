3907198c3f65625c41556a30e3bdfef751c74f17
Sultan Singh Atwal <58763741+atwalsultan@users.noreply.github.com>
2022-10-12 13:02:20 -0700

    #75 chore: code cleanup and maintenance (#76)

--- client/src/components/developer/cards/ExperienceCard.js
+++ client/src/components/developer/cards/ExperienceCard.js
@@ -3,40 +3,63 @@ import { useState, useEffect } from "react";
 import TechList from "../lists/TechList";
 
 const ExperienceCard = ({ job }) => {
-    const from = new Date(job.from);
-    const to = new Date(job.to);
+  const from = new Date(job.from);
+  const to = new Date(job.to);
 
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-    const [hidden, setHidden] = useState(true);
+  const [hidden, setHidden] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setHidden(false);
-        }, 10)
-    }, [])
-   
-    return (
-        <div className={`card job-card${hidden ? " hidden" : ""}`}>
-            <div className="first-row">
-                <h3>{ job.company }</h3>
-                <h4>{ job.location }</h4>
-            </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setHidden(false);
+    }, 10);
+  }, []);
 
-            <div className="second-row">
-                <h4>{ job.role }</h4>
-                { (from.getMonth() === to.getMonth() && from.getFullYear() === to.getFullYear()) ? <h4>{ months[from.getMonth()]} { from.getFullYear() }</h4> : <h4>{ months[from.getMonth()]} { from.getFullYear() } - { months[to.getMonth()] } { to.getFullYear() }</h4> }
-            </div>
+  return (
+    <div className={`card job-card${hidden ? " hidden" : ""}`}>
+      <div className="first-row">
+        <h3>{job.company}</h3>
+        <h4>{job.location}</h4>
+      </div>
 
-            <div className="description">
-                <p>{ job.description }</p>
-            </div>
+      <div className="second-row">
+        <h4>{job.role}</h4>
+        {from.getMonth() === to.getMonth() &&
+        from.getFullYear() === to.getFullYear() ? (
+          <h4>
+            {months[from.getMonth()]} {from.getFullYear()} - {"Present"}
+          </h4>
+        ) : (
+          <h4>
+            {months[from.getMonth()]} {from.getFullYear()} -{" "}
+            {months[to.getMonth()]} {to.getFullYear()}
+          </h4>
+        )}
+      </div>
 
-            <ul className="tech-used">
-                <TechList list={ job.tech } />
-            </ul>
-        </div>
-    )
-}
+      <div className="description">
+        <p>{job.description}</p>
+      </div>
 
-export default ExperienceCard
+      <ul className="tech-used">
+        <TechList list={job.tech} />
+      </ul>
+    </div>
+  );
+};
+
+export default ExperienceCard;
