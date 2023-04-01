c1a61089d1f7b4f69c09b6b5c4ab4a523b965468
Sultan Atwal <sultan.singh.atwal@gmail.com>
2023-04-01 09:46:36 -0700

    chore: maintenance

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Personal Website
-Fully responsive, full stack website to showcase my work, experience, education, certifications and skills. It allows users to choose between a dark and a light theme. Visit the website at [https://atwalsultan.herokuapp.com/dev](https://atwalsultan.herokuapp.com/dev)
+
+Fully responsive, full stack website to showcase my work, experience, education, certifications and skills. It allows users to choose between a dark and a light theme.
--- server/routes/developer/contact.js
+++ server/routes/developer/contact.js
@@ -10,48 +10,57 @@ require("dotenv").config();
 // Create and configure transporter
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
-    }
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 // Send out mails
 router.post("/", urlEncodedParser, (req, res) => {
-    const mailOptions = {
-        to: process.env.EMAIL,
-        subject: `Sultan Singh Atwal | Message from ${req.body.firstName}${req.body.lastName && ' ' + req.body.lastName}`,
-        text: `Hi Sultan,\n${req.body.firstName}${req.body.lastName && ' ' + req.body.lastName} just submitted the contact form on your personal website. Here's what they wrote:\n\n\t"${req.body.message}"\n\nYou can reach out to them using the email they provided: ${req.body.email}`
-    };
+  const mailOptions = {
+    to: process.env.EMAIL,
+    subject: `Message from ${req.body.firstName}${
+      req.body.lastName && " " + req.body.lastName
+    }`,
+    text: `Hi,\n${req.body.firstName}${
+      req.body.lastName && " " + req.body.lastName
+    } just submitted the contact form on your personal website. Here's what they wrote:\n\n\t"${
+      req.body.message
+    }"\n\nYou can reach out to them using the email they provided: ${
+      req.body.email
+    }`,
+  };
 
-    // Send mail
-    transporter.sendMail(mailOptions, (error, info) => {
+  // Send mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+
+      // To submitter
+      const mailOptionsUser = {
+        to: req.body.email,
+        subject: `Message Received`,
+        text: `Hi ${req.body.firstName}${
+          req.body.lastName && " " + req.body.lastName
+        },\n\nThank you for your message!\n\nI will reach out to you as soon as possible. This thread is not monitored. Please do not reply to this email. If you have more questions/suggestions, you may submit the contact form again.\n\nRegards,\nJohn Doe`,
+      };
+
+      // Send mail
+      transporter.sendMail(mailOptionsUser, (error, info) => {
         if (error) {
-            console.log(error);
-        } 
-        else {
-            console.log('Email sent to Sultan: ' + info.response);
-
-            // To submitter
-            const mailOptionsUser = {
-                to: req.body.email,
-                subject: `Sultan Singh Atwal | Message Received`,
-                text: `Hi ${req.body.firstName}${req.body.lastName && ' ' + req.body.lastName},\n\nThank you for your message!\n\nI will reach out to you as soon as possible. This thread is not monitored. Please do not reply to this email. If you have more questions/suggestions, you may submit the contact form again.\n\nRegards,\nSultan Singh Atwal`
-            };
-
-            // Send mail
-            transporter.sendMail(mailOptionsUser, (error, info) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log(`Email sent to ${req.body.email}: ` + info.response);
-                }
-            });
+          console.log(error);
+        } else {
+          console.log(`Email sent to ${req.body.email}: ` + info.response);
         }
-    });
+      });
+    }
+  });
 
-    res.redirect('/dev/about');
+  res.redirect("/about");
 });
 
 module.exports = router;
